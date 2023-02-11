54d600e6ad8a121fa01a4eadf57ab6a3025ddee6
HugoCls <hugocls.pro@gmail.com>
2023-02-11 13:54:12 +0100

    docs: Readme changes.

---
+++ AI.py
@@ -0,0 +1,202 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fft import fft, fftfreq
+import keras
+import tensorflow as tf
+import openpyxl
+import os
+
+def random_tranche(N,X,Y):
+    L = len(X)
+    x0 = np.random.randint(0,L+1-N)
+    return(X[x0:x0+N],Y[x0:x0+N])
+
+def fill_in(curves):
+    for nom in([("casse1_",0),("neuf1_",1)]):
+        for j in range(1,6):
+            print(j)
+            with open(os.getcwd()+'\\data\\'+nom[0]+str(j)+'.csv', newline='') as f:
+                reader = csv.reader(f)
+                data = [tuple(row) for row in reader]
+            X,Y=[],[]
+
+            for i in range(2,len(data)):
+                (x,y)=data[i]
+                x=float(x)
+                y=float(y)
+                X.append(x)
+                Y.append(y)
+
+            X,Y=np.array(X),np.array(Y)
+            Y=Y-np.mean(Y)
+            # Number of samples in normalized_tone
+
+            for i in range(1000):
+                X2,Y2 = random_tranche(len(X)//2,X,Y)
+                SAMPLE_RATE= np.mean([X2[k+1]-X2[k] for k in range(len(X2)-1)])
+                #DURATION= X2[len(X2)-1]-X[0]
+                N = len(X2)
+                yf = fft(Y2)
+                yf = np.abs(np.real(yf))
+                xf = fftfreq(N, SAMPLE_RATE)
+                xf,yf = xf[0:len(xf)//30],yf[0:len(yf)//30]
+                curves.append(xf)
+                curves.append(yf)
+                curves.append(nom[1])
+
+def plus_proche(X,Y,x_cible,e):
+    k = int(np.round((x_cible-X[0])/e))
+    if(k < 0):
+        print("trop bas")
+        k = 0
+    if(k>=len(X)):
+        k = len(X)-1
+    return(Y[k])
+
+
+def diff_fft(k1,k2,C):
+    y1 = C[3*k1+1]
+    y2 = C[3*k2+1]
+    x1 = C[3*k1]
+    x2 = C[3*k2]
+    e1 = x1[1]-x1[0]
+    e2 = x2[1]-x2[0]
+    x = min(x1)
+    x_max = max(x1)
+    s = 0
+    k = 0
+    if(e1<e2):
+        while(x < x_max):
+            s+= abs(y1[k] - plus_proche(x2,y2,x,e2))
+            k =+ 1
+            x += e1
+    else:
+        while(x < x_max):
+            s+= abs(y2[k] - plus_proche(x1,y1,x,e1))
+            k =+ 1
+            x += e2
+    return(s)
+
+def fix_number_of_points(X,Y,n_points,x_min,x_max):
+    x = x_min
+    X2 = []
+    Y2 = []
+    e = np.mean([X[k+1]-X[k] for k in range(len(X)-1)])
+    i = 0
+    while(i<n_points):
+        i+=1
+        X2.append(x)
+        k = (x-X[0])/e
+        k_int = int(k)
+        if(k>=len(X)-1):
+            Y2.append(Y[k_int])
+        else:
+            Y2.append((Y[k_int]+ (k-k_int)*(Y[k_int+1]-Y[k_int])))
+        x += (x_max-x_min)/(n_points-1)
+    return(X2,Y2)
+
+
+
+def normalise(curves):
+    n_max = max([len(curves[3*k]) for k in range(len(curves)//3)])
+    colors = ['b','g','r','m']
+    for k in range(len(curves)//3):
+        (curves[3*k],curves[3*k+1]) = fix_number_of_points(curves[3*k].tolist(),curves[3*k+1].tolist(),n_max,np.min(curves[3*k]),200)
+        plt.plot(curves[3*k],curves[3*k+1],colors[curves[3*k+2]])
+    plt.show()
+
+
+def create_model(curves = []):
+    if(len(curves) == 0):
+        fill_in(curves)
+        normalise(curves)
+    n_max = max([len(curves[3*k]) for k in range(len(curves)//3)])
+
+
+    LAYERS = [keras.layers.Input(n_max,dtype="float32")]
+    for k in range(3):
+        LAYERS.append(keras.layers.Dense((64//(2**k)),activation="relu",dtype="float32"))
+    LAYERS.append(keras.layers.Dense(1,activation="sigmoid"))
+    model = tf.keras.Sequential(LAYERS)
+    model.summary()
+    model.compile(
+        optimizer=tf.optimizers.Adam(),
+        loss='mse',
+        metrics=[tf.keras.metrics.BinaryAccuracy()])
+    history = model.fit(
+        np.array([curves[3*k+1] for k in range(len(curves)//3)]),
+        np.array([curves[3*k+2] for k in range(len(curves)//3)]),
+        epochs=10,
+        verbose=1,
+        validation_split = 0.2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    L_estime = model.predict([curves[3*k+1] for k in range(len(curves)//3)]).tolist()
+
+    err = 0
+    for k in range(len(curves)//3):
+        if(k < len(curves)//6 and L_estime[k][0]>0.5):
+            err += 1
+        if(k>= len(curves)//6 and L_estime[k][0]<0.5):
+            err += 1
+            
+    print(err," erreur, sur ",len(curves)//3, " exemples")
+    model.save("mymodel")
+    
+    return(err,model,history)
+
+
+
+def analyse(lien):
+    model = keras.models.load_model(os.getcwd() + "\\data\\model")
+    n_inputs = model.input.shape[1]
+    if lien[-3:]=="csv":
+        with open(lien, newline='') as f:
+            reader = csv.reader(f)
+            data = [tuple(row) for row in reader]
+        X,Y=[],[]
+        for i in range(2,len(data)):
+            (x,y)=data[i]
+            x=float(x)
+            y=float(y)
+            X.append(x)
+            Y.append(y)
+            
+        X,Y=np.array(X),np.array(Y)
+ 
+    else:
+        wb = openpyxl.load_workbook(lien)
+        ws = wb.active
+        X,Y=[],[]
+        for row in ws.iter_rows(values_only=True):
+            if row[0]!=None and row[0]>=1:
+                (x,y)=(row[1],row[2])
+                #print(x,y)
+                x=float(x)
+                y=float(y)
+                X.append(x)
+                Y.append(y)
+                
+        X,Y=np.array(X),np.array(Y)
+
+    X,Y=np.array(X),np.array(Y)
+    Y=Y-np.mean(Y)
+    SAMPLE_RATE= np.mean([X[k+1]-X[k] for k in range(len(X)-1)])
+    #DURATION= X[len(X)-1]-X[0]
+    N = len(X)
+    yf = fft(Y)
+    yf = np.abs(np.real(yf))
+    xf = fftfreq(N, SAMPLE_RATE)
+    xf,yf = xf[0:len(xf)//30],yf[0:len(yf)//30]
+    plt.plot(xf,yf,linewidth=0.4)
+    plt.savefig("new_img.png")
+    xf,yf = fix_number_of_points(xf.tolist(),yf.tolist(),n_inputs,0,200)
+    resultat = model.predict([yf])[0][0]
+    return(resultat)
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Using this voltage only, we want to know what state the fans in the duct are/wil
 The acquisition was done using a dantec and we recovered .csv files that we could then use
 
 ### Data processing
-We can easily get a list of tuples containing (t,U) as here `data=[('0.0001', '1.0684'), ... ,('0.0999', '1.0523'), ('0.1', '1.0602')]`
+We can easily get a list of tuples containing (t,U) as here `data=[('0.0001', '1.0684'), ... ,('0.0999', '1.0523'), ('0.1', '1.0602')]`.
 
 With `csv.reader`function:
 
@@ -35,9 +35,108 @@ xf = fftfreq(N, SAMPLE_RATE)
 ```
 Such parameters are based on the number of points in the file because we had some issues with our acquisition card which sometimes made acquisitions with non-constant numbers of points.
 
+We can easily see now that when comparing both ffts of a broken fan and a clean one,  we can see new frequences 
 ![My Image](images/README_IMAGES/differencing_ffts.png)
 
+And when a blade is removed from the fan, we most of the time see frenquency lines at 50Hz:
+
+![My Image](images/README_IMAGES/zoom_on_broken_ffts.png)
+
+Unfortunately, this is not at simple as that and knowing that degradations do not happen suddenly and as our role is to predict themand not to detect them, we decided to use an artificial intelligence model. 
+
 ## The place of AI in the project
+
+The principle is to present cases of degradation to a neural network so that it can train itself and then anticipate a type of degradation.
+
+### Train a model
+
+For the first AI model we wanted something simple: You enter a curve (points list) and you get as a binary output, if the fan was broken/damaged or not.
+
+#### Choice of model type
+
+Our first LAYER is an **input** with the **number of points of an ascquisition**.
+```Python
+LAYERS = [keras.layers.Input(n_max,dtype="float32")]
+```
+Then we add some LAYERS to train:
+```Python
+for k in range(3):
+    LAYERS.append(keras.layers.Dense((64//(2**k)),activation="relu",dtype="float32"))
+```
+
+Finally ,the last LAYER had to be a sigmoid to get our **binary output**.
+
+```Python
+LAYERS.append(keras.layers.Dense(1,activation="sigmoid"))
+```
+
+```Python
+model = tf.keras.Sequential(LAYERS)
+model.summary()
+model.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss='mse',
+    metrics=[tf.keras.metrics.BinaryAccuracy()])
+
+```
+**Adam** is a classical choice for *Stochastic optimisation*.(the model makes random oriented modifications that it chooses according to the results it obtains on its own calculations)
+
+About `mse` loss choice: **MSE** ensures that our trained model does not have outliers with huge errors, as MSE gives more weight to these errors due to the squared function.
+
+Disadvantage: if our model makes a single very bad prediction, the squared part of the function amplifies the error. 
+
+In our case it is to be avoided that a broken fan is not detected as such, so the MSE error seems very relevant 
+
+
+#### Mutiply the amount of data
+
+
+
+#### Generate the model
+```Python
+def create_model(curves = []):
+    if(len(curves) == 0):
+        remplir(curves)
+        normaliser(curves)
+    n_max = max([len(curves[3*k]) for k in range(len(curves)//3)])
+
+
+    LAYERS = [keras.layers.Input(n_max,dtype="float32")]
+    for k in range(3):
+        LAYERS.append(keras.layers.Dense((64//(2**k)),activation="relu",dtype="float32"))
+    LAYERS.append(keras.layers.Dense(1,activation="sigmoid"))
+    model = tf.keras.Sequential(LAYERS)
+    model.summary()
+    model.compile(
+        optimizer=tf.optimizers.Adam(),
+        loss='mse',
+        metrics=[tf.keras.metrics.BinaryAccuracy()])
+    history = model.fit(
+        np.array([curves[3*k+1] for k in range(len(curves)//3)]),
+        np.array([curves[3*k+2] for k in range(len(curves)//3)]),
+        epochs=10,
+        verbose=1,
+        validation_split = 0.2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    L_estime = model.predict([curves[3*k+1] for k in range(len(curves)//3)]).tolist()
+
+    err = 0
+    for k in range(len(curves)//3):
+        if(k < len(curves)//6 and L_estime[k][0]>0.5):
+            err += 1
+        if(k>= len(curves)//6 and L_estime[k][0]<0.5):
+            err += 1
+    print(err," erreur, sur ",len(curves)//3, " exemples")
+    model.save("mymodel")
+    return(err,model,history)
+```
 
 ![My Image](images/README_IMAGES/model_loss.png)
 
